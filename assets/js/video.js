1369fbc0a9c07942e00bcd6547ec7c8216329ec8
Kevin Plattret <kevin@plattret.com>
2020-02-19 18:25:25 +0000

    Creating the channel
    Includes a channel definition in our `UserSocket`. Adds the
    `VideoChannel` module that will handle that specific channel. Updates
    the JS code to join the channel from the client.

--- assets/js/video.js
+++ assets/js/video.js
@@ -4,17 +4,24 @@ let Video = {
   init(socket, element){ if(!element){ return }
     let playerId = element.getAttribute("data-player-id")
     let videoId = element.getAttribute("data-id")
+
     socket.connect()
+
     Player.init(element.id, playerId, () => {
       this.onReady(videoId, socket)
     })
   },
+
   onReady(videoId, socket){
     let msgContainer = document.getElementById("msg-container")
     let msgInput = document.getElementById("msg-input")
     let postButton = document.getElementById("msg-submit")
     let vidChannel = socket.channel("videos:" + videoId)
-    // TODO join the vidChannel
+
+    vidChannel.join()
+      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("error", reason => console.log("join failed", reason))
   }
 }
+
 export default Video
